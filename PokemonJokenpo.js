2b6552aa8f962fcfc89b4a8ae1b01ca8d9bafb2b
Carolina Tavares <c.pinheiro.t@gmail.com>
2021-10-04 20:00:35 -0300

    CSS version

---
+++ PokemonJokenpo.js
@@ -0,0 +1,351 @@
+
+// const pokemonTypes = ['bug', 'dark', 'dragon', 'eletric', 'fairy', 'fighthing', 'fire', 'flying', 'ghost', 
+//'grass', 'ground', 'ice', 'normal', 'poison','psychic', 'rock', 'water', 'steal']
+
+const pokemonTypes = [
+  {
+  type: 'bug', 
+  pokemonImg: ['./Images/bug/012.png', './Images/bug/015.png', './Images/bug/049.png', './Images/bug/123.png', './Images/bug/012.png', './Images/bug/166.png', './Images/bug/214.png', './Images/bug/284.png', './Images/bug/415.png'],
+  typeImg: './Images/Icons/bug.svg'
+  } , 
+
+{
+  type: 'dragon', 
+  pokemonImg: ['./Images/dragon/148.png','./Images/dragon/149.png', './Images/dragon/230.png','./Images/dragon/330.png','./Images/dragon/334.png', './Images/dragon/373.png', './Images/dragon/706.png', './Images/dragon/714.png'],
+  typeImg: './Images/Icons/dragon.svg'
+},
+
+{
+  type: 'dark', 
+  pokemonImg: ['./Images/dark/197.png', './Images/dark/215.png','./Images/dark/261.png', './Images/dark/359.png', './Images/dark/509.png', './Images/dark/510.png', './Images/dark/570.png', './Images/dark/827.png'],
+  typeImg: './Images/Icons/dark.svg'
+},
+
+{
+  type: 'electric', 
+  pokemonImg: ['./Images/electric/025.png', './Images/electric/026.png', './Images/electric/026_f2.png', './Images/electric/135.png',  './Images/electric/179.png', './Images/electric/181.png', './Images/electric/587.png', './Images/electric/702.png', './Images/electric/835.png', './Images/electric/877.png',],
+  typeImg: './Images/Icons/electric.svg'
+},
+
+{
+  type: 'fairy', 
+  pokemonImg: ['./Images/fairy/040.png', './Images/fairy/078_f2.png', './Images/fairy/176.png',  './Images/fairy/183.png', './Images/fairy/282.png', './Images/fairy/303.png', './Images/fairy/683.png', './Images/fairy/700.png',  './Images/fairy/764.png', './Images/fairy/778.png', './Images/fairy/869.png'],
+  typeImg: './Images/Icons/fairy.svg'
+ },
+
+{
+ type: 'fighting', 
+ pokemonImg: ['./Images/Fighting/106.png',
+  './Images/Fighting/107.png', './Images/Fighting/237.png', './Images/Fighting/257.png', './Images/Fighting/448.png', './Images/Fighting/620.png', './Images/Fighting/783.png', './Images/Fighting/802.png'],
+ typeImg: './Images/Icons/fighting.svg'
+},
+
+{
+ type: 'fire', 
+ pokemonImg: ['./Images/Fire/004.png', './Images/Fire/006.png', './Images/Fire/037.png', './Images/Fire/038.png', './Images/Fire/058.png', './Images/Fire/077.png', './Images/Fire/136.png', './Images/Fire/156.png', './Images/Fire/653.png', './Images/Fire/813.png', './Images/Fire/815.png',],
+typeImg: './Images/Icons/fire.svg'
+},
+
+{
+  type: 'flying', 
+  pokemonImg: ['./Images/Flying/006.png', './Images/Flying/012.png', './Images/Flying/018.png', './Images/Flying/123.png', './Images/Flying/164.png', './Images/Flying/166.png', './Images/Flying/267.png', './Images/Flying/334.png', './Images/Flying/587.png', './Images/Flying/714.png', './Images/Flying/733.png'],
+ typeImg: './Images/Icons/flying.svg'
+ },
+ 
+ {
+  type: 'ghost', 
+  pokemonImg: ['./Images/Ghost/094.png', './Images/Ghost/105_f2.png', './Images/Ghost/425.png', './Images/Ghost/429.png', './Images/Ghost/478.png', './Images/Ghost/607.png', './Images/Ghost/802.png', './Images/Ghost/897.png'],
+ typeImg: './Images/Icons/ghost.svg'
+ },
+
+ {
+  type: 'grass', 
+  pokemonImg: ['./Images/Grass/001.png', './Images/Grass/045.png', './Images/Grass/152.png', './Images/Grass/182.png', './Images/Grass/252.png', './Images/Grass/315.png', './Images/Grass/470.png', './Images/Grass/585.png', './Images/Grass/586.png', './Images/Grass/754.png'],
+ typeImg: './Images/Icons/grass.svg'
+ },
+
+ {
+  type: 'ground', 
+  pokemonImg: ['./Images/Ground/027.png', './Images/Ground/028.png', './Images/Ground/104.png', './Images/Ground/112.png', './Images/Ground/231.png', './Images/Ground/246.png', './Images/Ground/330.png', './Images/Ground/552.png', './Images/Ground/749.png'],
+typeImg: './Images/Icons/ground.svg'
+ },
+
+ {
+  type: 'ice', 
+  pokemonImg: ['./Images/Ice/038_f2.png', './Images/Ice/087.png', './Images/Ice/131.png', './Images/Ice/215.png', './Images/Ice/363.png', './Images/Ice/471.png', './Images/Ice/478.png', './Images/Ice/699.png'],
+typeImg: './Images/Icons/ice.svg'
+ },
+
+ {
+  type: 'normal', 
+  pokemonImg: ['./Images/Normal/017.png', './Images/Normal/052.png', './Images/Normal/053.png', './Images/Normal/115.png', './Images/Normal/133.png', './Images/Normal/143.png', './Images/Normal/164.png', './Images/Normal/216.png', './Images/Normal/241.png', './Images/Normal/298.png', './Images/Normal/300.png', './Images/Normal/335.png', './Images/Normal/572.png', './Images/Normal/831.png'],
+typeImg: './Images/Icons/normal.svg'
+ },
+
+
+ {
+  type: 'poison', 
+  pokemonImg: ['./Images/Poison/002.png', './Images/Poison/015.png', './Images/Poison/024.png', './Images/Poison/029.png', './Images/Poison/032.png', './Images/Poison/045.png', './Images/Poison/049.png', './Images/Poison/315.png'],
+typeImg: './Images/Icons/poison.svg'
+ },
+
+ {
+  type: 'psychic', 
+  pokemonImg: ['./Images/Psychic/150.png', './Images/Psychic/151.png', './Images/Psychic/196.png', './Images/Psychic/203.png', './Images/Psychic/249.png', './Images/Psychic/282.png', './Images/Psychic/358.png', './Images/Psychic/648.png', './Images/Psychic/678.png', './Images/Psychic/678_f2.png', './Images/Psychic/876.png'],
+typeImg: './Images/Icons/psychic.svg'
+ },
+
+ {
+  type: 'rock', 
+  pokemonImg: ['./Images/Rock/112.png', './Images/Rock/246.png', './Images/Rock/408.png', './Images/Rock/566.png', './Images/Rock/698.png', './Images/Rock/719.png', './Images/Rock/744.png', './Images/Rock/745.png'],
+typeImg: './Images/Icons/rock.svg'
+ },
+
+ {
+  type: 'steel', 
+  pokemonImg: ['./Images/Steel/212.png', './Images/Steel/227.png', './Images/Steel/304.png', './Images/Steel/385.png', './Images/Steel/448.png', './Images/Steel/707.png', './Images/Steel/777.png'],
+typeImg: './Images/Icons/steel.svg'
+ },
+
+ {
+  type: 'water', 
+  pokemonImg: ['./Images/Water/007.png', './Images/Water/008.png', './Images/Water/055.png', './Images/Water/116.png', './Images/Water/134.png', './Images/Water/158.png', './Images/Water/184.png', './Images/Water/258.png', './Images/Water/350.png', './Images/Water/418.png', './Images/Water/656.png'],
+typeImg: './Images/Icons/water.svg'
+ }
+ 
+]
+
+let computerType;
+let playerType;
+
+const playerChoice = () => {
+
+  let typeOptions = '';
+  
+  pokemonTypes.map((options) => {
+  
+ // let randomNumber = parseInt(Math.random() * options.pokemonImg.length);
+ // <img src='${options.pokemonImg[randomNumber]}'></img>
+  
+  typeOptions += `
+  <input name='types' type='radio' id='${options.type}' value='${options.type}' class='fuckinginput'>
+    <div class='icon' >
+      <label for='${options.type}' class='plswork'>
+        <div id='${options.type}-div' class='icon-background'> 
+          <img src='${options.typeImg}' class='typeIcons'> 
+        </div>
+        <p id='label-${options.type}' class='type-name'>${options.type}</p>
+      </label>
+    </div> `
+  })
+
+  document.getElementById('playerOptions').innerHTML = typeOptions
+
+  getPlayerType()
+}
+
+const getPlayerType = () => {
+  const radioOptions = document.getElementsByName('types');
+
+  for (var i = 0; i < radioOptions.length; i++)
+    if (radioOptions[i].checked === true) {
+      return radioOptions[i].value;
+    }
+  
+}
+
+const computerMove = () => {
+  let randomNumber = parseInt(Math.random() * pokemonTypes.length)
+  computerType = pokemonTypes[randomNumber].type;
+  
+  showComputerMove()
+  winOrLose();
+}
+
+showComputerMove = () => {
+  document.getElementById('computerType').innerHTML = `The computer choose ${computerType} and the player ${getPlayerType()}`
+}
+
+const winOrLose = () => {
+  
+  playerType = getPlayerType()
+
+if (playerType === 'bug') {
+  if (computerType === 'fire' || computerType === 'flying' || computerType ==='rock') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } if (computerType === 'dark' || computerType === 'grass' || computerType === 'psychic') {
+    return document.getElementById('result').innerHTML = 'You win!'
+  } else {
+    return document.getElementById('result').innerHTML = 'It is a tie?'
+  }
+}
+
+if (playerType === 'dark') {
+  if (computerType === 'bug' || computerType === 'fairy' || computerType === 'fighthing') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } if (computerType === 'ghost' || computerType === 'psychic') {
+    return document.getElementById('result').innerHTML = 'You win!'
+  } else {
+    return document.getElementById('result').innerHTML = 'It is a tie?'
+  }
+}
+
+
+if (playerType === 'dragon') {
+  if (computerType === 'ice' || computerType === 'fairy') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } 
+  // what the hell do i do with the fact that dragon is strong/weak against dragon?????
+}
+
+if (playerType === 'electric') {
+  if (computerType === 'ground') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } if (computerType === 'flying' || computerType === 'water') {
+    return document.getElementById('result').innerHTML = 'You win!'
+  } else {
+    return document.getElementById('result').innerHTML = 'It is a tie?'
+  }
+}
+
+if (playerType === 'fairy') {
+  if (computerType === 'steel' || computerType === 'poison') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } if (computerType === 'dark' || computerType === 'dragon' || computerType === 'fighthing') {
+    return document.getElementById('result').innerHTML = 'You win!'
+  } else {
+    return document.getElementById('result').innerHTML = 'It is a tie?'
+  }
+}
+
+if (playerType === 'fighthing') {
+  if (computerType === 'psychic' || computerType === 'flying' || computerType === 'fairy') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } if (computerType === 'dark' || computerType === 'ice' || computerType === 'steel' || computerType === 'rock' || computerType === 'normal') {
+    return document.getElementById('result').innerHTML = 'You win!'
+  } else {
+    return document.getElementById('result').innerHTML = 'It is a tie?'
+  }
+}
+
+if (playerType === 'fire') {
+  if (computerType === 'water' || computerType === 'ground' || computerType ==='rock') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } if (computerType === 'bug' || computerType === 'grass' || computerType === 'ice' || computerType === 'steel') {
+    return document.getElementById('result').innerHTML = 'You win!'
+  } else {
+    return document.getElementById('result').innerHTML = 'It is a tie?'
+  }
+}
+
+if (playerType === 'flying') {
+  if (computerType === 'rock' || computerType === 'ice' || computerType === 'electric') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } if (computerType === 'bug' || computerType === 'grass' || computerType === 'fighthing') {
+    return document.getElementById('result').innerHTML = 'You win!'
+  } else {
+    return document.getElementById('result').innerHTML = 'It is a tie?'
+  }
+}
+
+if (playerType === 'ghost') {
+  if (computerType === 'dark') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } if (computerType === 'psychic') {
+    return document.getElementById('result').innerHTML = 'You win!'
+  } else {
+    return document.getElementById('result').innerHTML = 'It is a tie?'
+
+    //'ghost' is also weak/strong against 'ghost' =_= 
+  }
+}
+
+if (playerType === 'grass') {
+  if (computerType === 'fire' || computerType === 'flying' || computerType === 'bug' || computerType === 'poison' || computerType === 'ice') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } if (computerType === 'ground' || computerType === 'rock' || computerType === 'water') {
+    return document.getElementById('result').innerHTML = 'You win!'
+  } else {
+    return document.getElementById('result').innerHTML = 'It is a tie?'
+  }
+}
+
+if (playerType === 'ground') {
+  if (computerType === 'water' || computerType === 'grass' || computerType === 'ice') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } if (computerType === 'electric' || computerType === 'rock' || computerType === 'fire' || computerType === 'poison' || computerType === 'steel') {
+    return document.getElementById('result').innerHTML = 'You win!'
+  } else {
+    return document.getElementById('result').innerHTML = 'It is a tie?'
+  }
+}
+
+if (playerType === 'ice') {
+  if (computerType === 'fire' || computerType === 'fighthing' || computerType === 'rock' || computerType === 'steel') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } if (computerType === 'dragon' || computerType === 'ice' || computerType === 'flying' || computerType === 'ground') {
+    return document.getElementById('result').innerHTML = 'You win!'
+  } else {
+    return document.getElementById('result').innerHTML = 'It is a tie?'
+  }
+}
+
+if (playerType === 'normal') {
+  if (computerType === 'fighthing') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } else {
+    return document.getElementById('result').innerHTML = 'It is a tie?'
+  }
+}
+
+if (playerType === 'poison') {
+  if (computerType === 'psychic' || computerType === 'ground') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } if (computerType === 'fairy' || computerType === 'grass') {
+    return document.getElementById('result').innerHTML = 'You win!'
+  } else {
+    return document.getElementById('result').innerHTML = 'It is a tie?'
+  }
+}
+
+if (playerType === 'psychic') {
+  if (computerType === 'dark'|| computerType === 'bug' || computerType === 'poison') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } if (computerType === 'poison' || computerType === 'fighthing') {
+    return document.getElementById('result').innerHTML = 'You win!'
+  } else {
+    return document.getElementById('result').innerHTML = 'It is a tie?'
+  }
+}
+
+if (playerType === 'rock') {
+  if (computerType === 'water' || computerType === 'steel' || computerType === 'ground' || computerType === 'grass' || computerType === 'fighthing') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } if (computerType === 'bug' || computerType === 'fire' || computerType === 'flying' || computerType === 'ice') {
+    return document.getElementById('result').innerHTML = 'You win!'
+  } else {
+    return document.getElementById('result').innerHTML = 'It is a tie?'
+  }
+}
+
+if (playerType === 'water') {
+  if (computerType === 'grass' || computerType === 'eletric') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } if (computerType === 'fire' || computerType === 'rock' || computerType === 'ground') {
+    return document.getElementById('result').innerHTML = 'You win!'
+  } else {
+    return document.getElementById('result').innerHTML = 'It is a tie?'
+  }
+}
+
+if (playerType === 'steel') {
+  if (computerType === 'fire' || computerType === 'fighthing' || computerType === 'ground') {
+    return document.getElementById('result').innerHTML = 'You lose!'
+  } if (computerType === 'fairy' || computerType === 'rock' || computerType === 'ice') {
+    return document.getElementById('result').innerHTML = 'You win!'
+  } else {
+    return document.getElementById('result').innerHTML = 'It is a tie?'
+  }
+}
+
+}
